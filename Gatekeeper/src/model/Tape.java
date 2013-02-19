31e24f5039414331d7e3b37a383dcdc163a07567
MarnBeast <mbdillon88+GIT@gmail.com>
2013-02-18 23:03:02 -0500

    Significant progress on Tape class. I think it's about done, but I'm sure there will be revisions as I test.

--- Gatekeeper/src/model/Tape.java
+++ Gatekeeper/src/model/Tape.java
@@ -1,28 +1,38 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.InvalidPathException;
 import java.util.ArrayList;
+import javafx.scene.media.Media;
 
 
-public class Tape {	
+public class Tape implements Serializable
+{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private String name;
 	
-	private IDListComparable<String> typesIdList;
+	final private String[] defaultTypes = new String[]
+	{ "Intro", "Ending", "Filler" };
 	
-	private ArrayList<Clip> introsArrayList;
-	private ArrayList<Clip> endingsArrayList;
-	private ArrayList<Clip> fillersArrayList;
+	private IDListComparable<String> allTypesIdList;
 	private ArrayList<Clip> miscArrayList;
 	
 	private Settings defaultSettings;
-	
+
 	
 	public Tape()
 	{
-		typesIdList = new IDListComparable<String>();
-		
-		introsArrayList = new ArrayList<Clip>();
-		endingsArrayList = new ArrayList<Clip>();
-		fillersArrayList = new ArrayList<Clip>();
+		allTypesIdList = new IDListComparable<String>();
+		allTypesIdList.addValues(defaultTypes);
 		miscArrayList = new ArrayList<Clip>();
 		
 		defaultSettings = new Settings();
@@ -44,88 +54,87 @@ public class Tape {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
+	
+	
+	
+	
+	// Manage Type IDs
+	
 	/**
-	 * @return the typesIdList
+	 * Returns a list containing all of the type strings added to this tape.
+	 * @return the list of type strings associated with this tape.
 	 */
-	public IDListComparable<String> getTypesIdList() {
-		return typesIdList;
+	public ArrayList<String> getTypes()
+	{
+		return allTypesIdList.values();
+	}
+	
+	/**
+	 * Adds the type string to the Tape types if it doesn't exist and returns the type ID
+	 * assigned to the type.
+	 * @param typeString The string associated with the given type.
+	 * @return the type ID assigned to the type.
+	 */
+	public int getTypeID(String typeString)
+	{
+		return allTypesIdList.getsetID(typeString);
 	}
 
 
-	/**
-	 * @param typesIdList the typesIdList to set
-	 */
-	public void setTypesIdList(IDListComparable<String> typesIdList) {
-		this.typesIdList = typesIdList;
+	
+	
+	// Add Clip Methods
+	
+	public Clip addClip(String videoClipPath)
+	{
+		Media media = new Media(videoClipPath);
+		Clip clip = new Clip(media, 0.0, media.durationProperty().get().toSeconds());
+		miscArrayList.add(clip);
+		return clip;
+	}
+
+	public Clip[] addClips(String[] videoClipPaths)
+	{
+		return addClips(videoClipPaths, false);
+	}
+	
+	public Clip[] addClips(String[] videoClipPaths, boolean calculateRelativeClipTimes)
+	{
+		Clip[] clips = new Clip[videoClipPaths.length];
+		double totalTime = 0.0;
+		for(int i = 0; i < videoClipPaths.length; i++)
+		{
+			Media media = new Media(videoClipPaths[i]);
+			if(calculateRelativeClipTimes)
+			{	
+				// startTime should be the total time accumulated up until this clip.
+				// We don't care about totalTime's value at this time because it will
+				// be changed later. Thus, totalTime + 1 is a placeholder.
+				clips[i] = new Clip(media, totalTime, totalTime + 1); 
+				miscArrayList.add(clips[i]);
+				totalTime += media.durationProperty().get().toSeconds();
+			}
+			else {
+				clips[i] = new Clip(media, 0.0, media.durationProperty().get().toSeconds()); 
+				miscArrayList.add(clips[i]);
+			}
+
+		}
+		
+		if(calculateRelativeClipTimes)
+		{
+			// Set the correct totalTime for all of these clips.
+			for(int i = 0; i < clips.length; i++)
+			{
+				clips[i].setPlacePercent(clips[i].getStartTime(), totalTime);
+			}
+		}
+		
+		return clips;
 	}
 
 
-	/**
-	 * @return the introsArrayList
-	 */
-	public ArrayList<Clip> getIntrosArrayList() {
-		return introsArrayList;
-	}
-
-
-	/**
-	 * @param introsArrayList the introsArrayList to set
-	 */
-	public void setIntrosArrayList(ArrayList<Clip> introsArrayList) {
-		this.introsArrayList = introsArrayList;
-	}
-
-
-	/**
-	 * @return the endingsArrayList
-	 */
-	public ArrayList<Clip> getEndingsArrayList() {
-		return endingsArrayList;
-	}
-
-
-	/**
-	 * @param endingsArrayList the endingsArrayList to set
-	 */
-	public void setEndingsArrayList(ArrayList<Clip> endingsArrayList) {
-		this.endingsArrayList = endingsArrayList;
-	}
-
-
-	/**
-	 * @return the fillersArrayList
-	 */
-	public ArrayList<Clip> getFillersArrayList() {
-		return fillersArrayList;
-	}
-
-
-	/**
-	 * @param fillersArrayList the fillersArrayList to set
-	 */
-	public void setFillersArrayList(ArrayList<Clip> fillersArrayList) {
-		this.fillersArrayList = fillersArrayList;
-	}
-
-
-	/**
-	 * @return the miscArrayList
-	 */
-	public ArrayList<Clip> getMiscArrayList() {
-		return miscArrayList;
-	}
-
-
-	/**
-	 * @param miscArrayList the miscArrayList to set
-	 */
-	public void setMiscArrayList(ArrayList<Clip> miscArrayList) {
-		this.miscArrayList = miscArrayList;
-	}
-
-
+	
 	/**
 	 * @return the defaultSettings
 	 */
@@ -140,6 +149,48 @@ public class Tape {
 	public void setDefaultSettings(Settings defaultSettings) {
 		this.defaultSettings = defaultSettings;
 	}
+
+
+	/**
+	 * 
+	 * @param filepath
+	 * @throws IOException 
+	 */
+	public void saveTape(String filepath) throws IOException
+	{
+		if(!filepath.endsWith(Constants.TAPE_EXTENSION))
+		{
+			filepath = filepath.concat(Constants.TAPE_EXTENSION);
+		}
+		FileOutputStream fileOut = new FileOutputStream(filepath);
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		out.writeObject(this);
+		out.close();
+		fileOut.close();
+	}
 	
+	/**
+	 * 
+	 * @param filepath
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws InvalidPathException
+	 */
+	public static Tape loadTape(String filepath) throws IOException, ClassNotFoundException
+	{
+		if(!filepath.endsWith(Constants.TAPE_EXTENSION))
+		{
+			throw new InvalidPathException(filepath, "The supplied filepath must point to a valid Gatekeeper tape file with " + 
+											Constants.TAPE_EXTENSION + " extension.");
+		}
+		FileInputStream fileIn = new FileInputStream(filepath);
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+
+		Tape tape = (Tape) in.readObject();
+		in.close();
+		fileIn.close();
+		return tape;
+	}
 	
 }
