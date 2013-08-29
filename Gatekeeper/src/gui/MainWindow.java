0d97f3b49de70f0d305399a43aa5cc4ca3e1a21e
MarnBeast <mbdillon88+GIT@gmail.com>
2013-08-28 23:09:40 -0400

    Experimenting with JavaFX and media player controls. I've got VideoPlayer pretty much working!

--- Gatekeeper/src/gui/MainWindow.java
+++ Gatekeeper/src/gui/MainWindow.java
@@ -1,8 +1,14 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application{
@@ -15,9 +21,64 @@ public class MainWindow extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Group root = new Group();
+		
+		String projectRootDirectory = getHostServices().getDocumentBase();
+		Media media = new Media(projectRootDirectory + "src/tests/Clip01.mp4");
+		
+		final VideoPlayer player = new VideoPlayer(media);
+		player.setWidth(200);
+		final VideoPlayer player2 = new VideoPlayer(media);
+		player2.setWidth(200);
+		final VideoPlayer player3 = new VideoPlayer(media);
+		player3.setWidth(200);
+		final VideoPlayer player4 = new VideoPlayer(media);
+		player4.setWidth(200);
+		
+		HBox hbox = new HBox();
+		HBox hbox2 = new HBox();
+		VBox vBox = new VBox();
+		hbox.getChildren().addAll(player, player2);//, player2, player3, player4);
+		hbox2.getChildren().addAll(player3, player4);//, player2, player3, player4);
+		hbox.setMargin(player, new Insets(5));
+		hbox.setMargin(player2, new Insets(5));
+		hbox2.setMargin(player3, new Insets(5));
+		hbox2.setMargin(player4, new Insets(5));
+		vBox.getChildren().addAll(hbox, hbox2);
+		
+		root.getChildren().add(vBox);
+		
 		Scene scene = new Scene(root, 600, 400);
 		primaryStage.setScene(scene);
+		scene.getStylesheets().add(MainWindow.class.getResource("GKStyle.css").toExternalForm());
+		//System.out.println(MainWindow.class.getResource("GKStyle.css"));
 		primaryStage.show();
+		
+		player.play();
+		player2.play();
+		player3.play();
+		player4.play();
+		
+		Thread thread = new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Thread.sleep(5000);
+				} catch (InterruptedException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				player.setWidth(500);
+				player2.setWidth(500);
+				player3.setWidth(500);
+				player4.setWidth(500);
+			}
+		});
+		
+		thread.start();
 	}
 
 }
