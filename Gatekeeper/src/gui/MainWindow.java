8338df3d43f15296b44fbd380187955f29a16962
martin <marti@DESKTOP-04GKEV0.fios-router.home>
2016-03-12 22:44:17 -0500

    Changed paths to match my new directory structure.

--- Gatekeeper/src/gui/MainWindow.java
+++ Gatekeeper/src/gui/MainWindow.java
@@ -37,51 +37,56 @@ public class MainWindow extends Application{
 	 * IN C:\Program Files\Java\jdk1.7.0_45\jre\lib YOU'LL FIND jfxrt.jar. ADD THAT!!!
 	 */
 
-	
+
 	private boolean paused = false;
 	private ArrayList<Tape> tapes;
 	private TimelinePlayer timelinePlayer;
-	
+
+
+	public static final String TEST_CLIPS_PATH = "C:\\Users\\marti\\Google Drive\\Stuff\\Games\\Tabletop\\Atmosfear\\Clips";
+
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		CleanUpTempDir();
-		//TimelinePlayerTest(primaryStage);
-		
+		TimelinePlayerTest(primaryStage);
+
 		//VideoPlayerTest(primaryStage);
-		
-		SerializeTestTape("C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Main Tape\\");
+
+//		SerializeTestTape("C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Main Tape\\");
 //		SerializeTestTape("C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Booster 1\\");
-		
+
 		//TimelineTest(new String[]{
 		//		"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Main Tape\\TestTape.gktape",
 		//		"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Booster 1\\TestTape.gktape"});
 		//		"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Main Tape\\Main Tape.gktape",
 		//		"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Booster 1\\Booster 1.gktape"});
 	}
-	
+
 	public void TimelinePlayerTest(final Stage primaryStage)
 	{
 		// Initialize Window
 		final Group root = new Group();
 		final Scene scene = new Scene(root, 200, 100);
-		
+
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add(MainWindow.class.getResource("GKStyle.css").toExternalForm());
 		//System.out.println(MainWindow.class.getResource("GKStyle.css"));
 		primaryStage.show();
-		
+
 		// Throw on a label
 		Label messageLabel = new Label("Building Timeline");
 		Label progressLabel = new Label("0");
 		VBox vBox = new VBox();
 		vBox.getChildren().addAll(messageLabel, progressLabel);
 		root.getChildren().add(vBox);
-		
+
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>(){
 			@Override
 			public void handle(WindowEvent arg0) {
@@ -97,16 +102,16 @@ public class MainWindow extends Application{
 				Platform.exit();
 			}
 		});
-		
+
 		// Built Timeline and run Timeline Player
 		Task<Timeline> timelineTask = new Task<Timeline>(){
 			@Override
 			protected Timeline call() throws Exception
 			{
 				String[] tapePaths = new String[]{
-						"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Main Tape\\TestTape.gktape",//};
-						"C:\\Users\\MarnBeast\\Videos\\atmosfear clips\\Booster 1\\TestTape.gktape"};
-								
+						TEST_CLIPS_PATH + "\\Main Tape\\TestTape.gktape",//};
+						TEST_CLIPS_PATH + "\\Booster 1\\TestTape.gktape"};
+
 				tapes = new ArrayList<>();
 				// Load Tapes - 50%
 				double progress = 0.0;
@@ -139,19 +144,19 @@ public class MainWindow extends Application{
 				for (Tape tape : tapes)
 				{
 					//tape.loadClipsMedia();
-					
+
 					gameSettings.addTapeIncludes(tape, EnumSet.of(
 							ClipBaseTypes.INTRO,
 							ClipBaseTypes.FILLER,
 							ClipBaseTypes.MISC,
 							ClipBaseTypes.END));
 				}
-				
+
 				// Build Timeline - 50%
 				updateMessage("Building Timeline");
 				updateProgress(50.0, 100.0);
 				TimelineBuilder tBuilder = new TimelineBuilder();
-				
+
 				TimelineBuilder.ProgressObserver pObserver = new TimelineBuilder.ProgressObserver()
 				{
 					@Override
@@ -162,26 +167,26 @@ public class MainWindow extends Application{
 				};
 
 				tBuilder.addTBProgressObserver(pObserver);
-				
+
 				Timeline timeline = tBuilder.createTimeline(tapes.toArray(new Tape[0]), gameSettings,
 						1200,
-						Constants.DEFAULT_TRANSITION_TIME);				
+						Constants.DEFAULT_TRANSITION_TIME);
 
 				updateMessage("Timeline Created!");
-				
+
 				return timeline;
 			}
 		};
-		
+
 		messageLabel.textProperty().bind(timelineTask.messageProperty());
 		progressLabel.textProperty().bind(timelineTask.progressProperty().multiply(100.0).asString());
-		
+
 		timelineTask.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
 			@Override
 			public void handle(WorkerStateEvent event)
 			{
 				final Timeline timeline = ((Task<Timeline>)event.getSource()).getValue();
-				
+
 				for(double clipTime : timeline.getClipTimes())
 				{
 					Clip clip = timeline.getClip(clipTime);
@@ -190,11 +195,11 @@ public class MainWindow extends Application{
 				TapePlayerWindow playerWindow = new TapePlayerWindow(timeline);
 				playerWindow.show();
 				playerWindow.getTimelinePlayer().play();
-				
+
 //				timelinePlayer = new TimelinePlayer(timeline);
 //				final TimelinePlayerControls playerControls = new TimelinePlayerControls(timelinePlayer);
 //				root.getChildren().add(playerControls);
-//				
+//
 //				timelinePlayer.play();
 //				timelinePlayer.setOnReady(new Runnable()
 //				{
@@ -208,8 +213,8 @@ public class MainWindow extends Application{
 //						double frameHeight = primaryStage.getHeight() - scene.getHeight();
 //						primaryStage.setWidth(width + frameWidth);
 //						primaryStage.setHeight(height + frameHeight);
-//						
-//						
+//
+//
 //						ChangeListener<Object> updateSizeListener = new ChangeListener<Object>()
 //						{
 //							@Override
@@ -218,7 +223,7 @@ public class MainWindow extends Application{
 //							{
 //								playerControls.setWidth(primaryStage.getWidth());
 //								playerControls.setHeight(primaryStage.getHeight());
-//								
+//
 //								//System.out.print();
 //							}
 //						};
@@ -229,12 +234,12 @@ public class MainWindow extends Application{
 			}
 
 		});
-		
-		new Thread(timelineTask).start();	
+
+		new Thread(timelineTask).start();
 	}
-	
+
 	/**
-	 * Must run this on application startup because JavaFX currently has a bug where the 
+	 * Must run this on application startup because JavaFX currently has a bug where the
 	 * Media object holds on to it's source file until the application is terminated.
 	 * I've got deleteOnExit running but it doesn't work all of the time.
 	 */
@@ -247,23 +252,23 @@ public class MainWindow extends Application{
 			}
 		}
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	/* HERE LIE OLD TESTS */
-	
-	
-	
-	
+
+
+
+
 	public void VideoPlayerTest(Stage primaryStage)
 	{
 		Group root = new Group();
-		
+
 		String projectRootDirectory = getHostServices().getDocumentBase();
 		Media media = new Media(projectRootDirectory + "src/tests/Clip01.mp4");
-		
+
 		final VideoPlayer player = new VideoPlayer(media);
 		//player.setHeight(200);
 		final VideoPlayer player2 = new VideoPlayer(media);
@@ -272,7 +277,7 @@ public class MainWindow extends Application{
 		//player3.setHeight(200);
 		final VideoPlayer player4 = new VideoPlayer(media);
 		//player4.setHeight(200);
-		
+
 		HBox hbox = new HBox();
 		HBox hbox2 = new HBox();
 		VBox vBox = new VBox();
@@ -283,9 +288,9 @@ public class MainWindow extends Application{
 		HBox.setMargin(player3, new Insets(5));
 		HBox.setMargin(player4, new Insets(5));
 		vBox.getChildren().addAll(hbox, hbox2);
-		
+
 		root.getChildren().add(vBox);
-		
+
 		Scene scene = new Scene(root, 600, 400);
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add(MainWindow.class.getResource("GKStyle.css").toExternalForm());
@@ -299,9 +304,9 @@ public class MainWindow extends Application{
 					Number arg1, Number arg2) {
 				System.out.println("Listener: " + player.getWidth());
 			}
-			
+
 		});
-		
+
 		Thread thread = new Thread(new Runnable()
 		{
 			@Override
@@ -330,7 +335,7 @@ public class MainWindow extends Application{
 				player2.setPlayerWidth(500);
 				player3.setPlayerWidth(500);
 				player4.setPlayerWidth(500);
-				
+
 				player2.play();
 				try {
 					Thread.sleep(2000);
@@ -339,12 +344,12 @@ public class MainWindow extends Application{
 					e.printStackTrace();
 				}
 				System.out.println(player.getWidth());
-				
+
 				player.setPlayerWidth(200);
 				player2.setPlayerWidth(200);
 				player3.setPlayerWidth(200);
 				player4.setPlayerWidth(200);
-				
+
 				player3.play();
 				try
 				{
@@ -357,11 +362,11 @@ public class MainWindow extends Application{
 				System.out.println(player.getWidth());
 			}
 		});
-		
+
 		thread.start();
 	}
-	
-	
+
+
 	public void SerializeTestTape(String sbasePath)
 	{
 		final String basePath = sbasePath;
@@ -387,26 +392,26 @@ public class MainWindow extends Application{
 				};
 				File[] listOfFiles = folder.listFiles(filter);
 				String[] filePaths = new String[listOfFiles.length];
-				
+
 				for (int i = 0; i < listOfFiles.length; i++)
 				{
-					filePaths[i]=listOfFiles[i].getPath(); 
+					filePaths[i]=listOfFiles[i].getPath();
 				}
 				Tape tape = new Tape();
 				tape.addClips(filePaths, ClipBaseTypes.MISC, true);
-				
+
 				Clip[] clips = tape.getClips(ClipBaseTypes.MISC);
-				
+
 				Clip clip = clips[0];										// Intro
 				tape.removeClip(clip, ClipBaseTypes.MISC);
 				tape.addClip(clip, ClipBaseTypes.INTRO);
 				clip.addType(Constants.DEFAULT_TYPES[1]);
-				
+
 				clip = clips[clips.length-1];								// Ending
 				tape.removeClip(clip, ClipBaseTypes.MISC);
 				tape.addClip(clip, ClipBaseTypes.END);
 				clip.addType(Constants.DEFAULT_TYPES[2]);
-				
+
 				for(int i = 1; i < clips.length-1; i++)
 				{
 					if(i%2 > 0)
@@ -417,22 +422,22 @@ public class MainWindow extends Application{
 						clip.addType(Constants.DEFAULT_TYPES[3]);
 					}
 				}
-				
+
 				clips[16].addType("Soul Rangers");
 				clips[22].addType("Soul Rangers Release");
 				clips[10].addType("Black Hole Release");
 				clips[56].addType("Black Hole Release");
 				clips[81].addType("Black Hole Release");
-				
+
 		//		clips[18].addType("Soul Rangers");
 		//		clips[20].addType("Soul Rangers Release");
 		//		clips[16].addType("Black Hole Release");
 		//		clips[36].addType("Black Hole Release");
-				
+
 				Settings settings = new Settings();
 				settings.addLandmark("Soul Rangers", 600);
 				settings.addLandmark("Soul Rangers Release", 800);
-				
+
 				try
 				{
 					tape.saveTape(basePath + "TestTape" + Constants.TAPE_EXTENSION);
@@ -445,10 +450,10 @@ public class MainWindow extends Application{
 				}
 			}
 		});
-		
+
 		thread.start();
 	}
-	
+
 	public Timeline TimelineTest(String[] tapePaths)
 	{
 		ArrayList<Tape> tapes = new ArrayList<>();
@@ -476,27 +481,27 @@ public class MainWindow extends Application{
 		for (Tape tape : tapes)
 		{
 			tape.loadClipsMedia();
-			
+
 			gameSettings.addTapeIncludes(tape, EnumSet.of(
 					ClipBaseTypes.INTRO,
 					ClipBaseTypes.FILLER,
 					ClipBaseTypes.MISC,
 					ClipBaseTypes.END));
 		}
-		
+
 		Timeline timeline = new TimelineBuilder().createTimeline(
 				tapes.toArray(new Tape[0]),
 				gameSettings,
 				Constants.DEFAULT_TOTAL_GAME_TIME,
 				Constants.DEFAULT_TRANSITION_TIME);
-		
+
 		return timeline;
 //		for (Entry<Double, Clip> clipTime: timeline)
 //		{
 //			Double time = clipTime.getKey();
 //			Clip clip = clipTime.getValue();
 //			String vidPath = clip.getVideo().getSource();
-//			
+//
 //			String typesString = "";
 //			for (String type : clip.getTypes())
 //			{
@@ -505,7 +510,7 @@ public class MainWindow extends Application{
 //					typesString += type + " ";
 //				}
 //			}
-//			
+//
 //			//String[] splitPath = vidPath.split("/");
 //			//vidPath = splitPath[splitPath.length-2] + "/" + splitPath[splitPath.length-1];
 //			System.out.println(time + "\t" + typesString + "\t" + vidPath);
